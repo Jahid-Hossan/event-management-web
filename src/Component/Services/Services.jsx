295d98e4494ddbb75b8e6cc1b72ebd38a87b0914
jahid433 <jahidhossen02@gmail.com>
2023-10-06 21:42:06 +0300

    added all services, applyed style and added read more button for more details

--- src/Component/Services/Services.jsx
+++ src/Component/Services/Services.jsx
@@ -1,13 +1,23 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../../Provider/AuthProvider';
+import Service from '../Service/Service';
 
 const Services = () => {
 
-    const allData = useContext(AuthContext)
+    const { allData } = useContext(AuthContext);
+    console.log(allData)
 
     return (
         <div>
-
+            <div className='my-5 md:my-10 text-center'>
+                <h2 className='text-2xl font-bold'>EVENT MANAGEMENT SERVICES</h2>
+                <p>We organize all type of social events</p>
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                {
+                    allData.map(service => <Service key={service.id} service={service}></Service>)
+                }
+            </div>
         </div>
     );
 };
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../../Component/Banner/banner';
+import Services from '../../Component/Services/Services';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Services></Services>
         </div>
     );
 };
